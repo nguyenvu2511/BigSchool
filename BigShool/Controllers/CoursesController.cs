a4373a0efb4cdc337a5e12d216c60def0aa9dd95
nguyenvu2511 <59417691+nguyenvu2511@users.noreply.github.com>
2021-07-20 22:54:09 +0700

    new

--- BigShool/Controllers/CoursesController.cs
+++ BigShool/Controllers/CoursesController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -43,7 +44,7 @@ namespace BigShool.Controllers
             ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
             var listAtt = context.Attendances.Where(p => p.Attendee == user.Id).ToList();
             var course = new List<Course>();
-            foreach(Attendance temp in listAtt)
+            foreach (Attendance temp in listAtt)
             {
                 Course course1 = temp.Course;
                 course1.Name = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(course1.LectureId).Name;
@@ -58,11 +59,56 @@ namespace BigShool.Controllers
             BigSchoolContext context = new BigSchoolContext();
             ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
             var course = context.Courses.Where(c => c.LectureId == user.Id && c.Dateime > DateTime.Now).ToList();
-            foreach(Course i in course)
+            foreach (Course i in course)
             {
                 i.Name = user.Name;
             }
             return View(course);
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            Course cs = context.Courses.Where(p => p.Id == id).FirstOrDefault() ;
+            if(cs == null)
+            {
+                Response.StatusCode = 404;
+                return null; 
+            }
+            return View(cs);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult Xacnhanxoa(int id)
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            Course cs = context.Courses.Where(n => n.Id == id).FirstOrDefault();
+            context.Courses.Remove(cs);
+            context.SaveChanges();
+            return RedirectToAction("Mine");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            Course cs = context.Courses.Where(p => p.Id == id).FirstOrDefault();
+            cs.ListCategory = context.Categories.ToList();
+            if (cs == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(cs);
+        }
+        [HttpPost, ActionName("Edit")]
+        public ActionResult Xacnhansua(int id)
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            Course cs = context.Courses.FirstOrDefault(n => n.Id == id);
+            UpdateModel(cs);;
+            context.SaveChanges();
+            return RedirectToAction("Mine");
         }
     }
 }
